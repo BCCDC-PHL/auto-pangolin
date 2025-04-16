f80c5953315912b4a83cf84f0a73aa20f10dc185
Dan Fornika <dfornika@gmail.com>
2025-04-16 14:41:50 -0700

    Launch nextflow from isolated working dir

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='auto-pangolin',
-    version='0.1.0-alpha-1',
+    version='0.1.0',
     packages=find_namespace_packages(),
     entry_points={
         "console_scripts": [
@@ -15,7 +15,7 @@ setup(
     },
     install_requires=[
     ],
-    description=' Automated lineage assignment of SARS-CoV-2 sequence data using Pangolin',
+    description='Automated lineage assignment of SARS-CoV-2 sequence data using Pangolin',
     url='https://github.com/BCCDC-PHL/auto-pangolin',
     author='Dan Fornika',
     author_email='dan.fornika@bccdc.ca',
